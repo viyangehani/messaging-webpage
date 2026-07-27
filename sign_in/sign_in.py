e044077c049e55a59868fa6852389c6afb1ddb2b
viyangehani <viyangehani16@gmail.com>
2026-07-27 14:03:52 -0700

    Remove user's chats from session state
    Remove saving user's chats from session state.

--- sign_in/sign_in.py
+++ sign_in/sign_in.py
@@ -6,7 +6,6 @@ df=pd.read_csv("identity.csv")
 def switch_page(name_of_user,chat_list):
   # Save your string and dataframe into labeled memory boxes
   st.session_state["user's name"] = name_of_user
-  st.session_state["user's chats"] = chat_list
 
   # Trigger the transition
   st.session_state.script = "user_chat.py"
