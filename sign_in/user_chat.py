db290ad23a7c5ae462283c36e3a45ebb560566ec
viyangehani <viyangehani16@gmail.com>
2026-07-27 13:47:22 -0700

    Update session state key for text received

--- sign_in/user_chat.py
+++ sign_in/user_chat.py
@@ -1,5 +1,4 @@
 import pandas as pd
 from ipywidgets import Button, Output, Text, VBox
 from IPython.display import display
-text_received = st.session_state["shared_string"]
-df_received = st.session_state["shared_df"]
+text_received = st.session_state["user's name"]
