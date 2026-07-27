a483791a16e071bed88ec1b4a471380978424ee5
viyangehani <viyangehani16@gmail.com>
2026-07-27 09:32:42 -0700

    Add output widget and load identity CSV

--- sign_in/sign_in.py
+++ sign_in/sign_in.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from ipywidgets import Button, Output, Text, VBox
 from IPython.display import display
+output=Output()
+df=pd.read_csv("identity.csv")
 def switch_page(name_of_user,chat_list):
   # Save your string and dataframe into labeled memory boxes
   st.session_state["user's name"] = name_of_user
