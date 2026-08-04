edfa9dca385b2cd93656a4152dadffd9661ded79
viyangehani <viyangehani16@gmail.com>
2026-08-04 11:06:38 -0700

    Integrate Tkinter for user message input
    Refactor user chat functionality to use Tkinter for GUI input and datetime for message timestamping.

--- sign_in/user_chat.py
+++ sign_in/user_chat.py
@@ -1,7 +1,18 @@
 import pandas as pd
-from ipywidgets import Button, Output, Text, VBox
-from IPython.display import display
-import time
-text_received = st.session_state["user's name"]
+import tkinker as tk
+from datetime import datetime
+name = st.session_state["user's name"]
 df=pd.read_csv("messages.csv")
-info={"name":
+
+def handle_submit():
+  global df
+  global name
+  message = user_entry.get()
+  time = datetime.now()
+  new_message = {"user":name, "message":message, "time":time}
+  message_send = pd.DataFrame([new_message])
+  df=pd.concat([df, message_send], ignore_index = False )
+  
+  user_entry.delete(0, tk.END)
+
+root = tk.Tk() 
